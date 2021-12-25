71c6a0e8c431502588e8269048af21809d47b0f6
“ZhangGuoLong666” <2507914174@qq.com>
2021-12-25 21:14:56 +0800

    将nacos集成入项目

--- src/main/java/com/zgl/ZglAuthApplcation.java
+++ src/main/java/com/zgl/ZglAuthApplcation.java
@@ -3,11 +3,11 @@ package com.zgl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 @MapperScan("com.zgl.mapper")
 public class ZglAuthApplcation {
 
